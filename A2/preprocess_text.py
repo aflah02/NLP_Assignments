5168d2416e9d1c96045618cfbd549fd8b876bd69
Aflah <72096386+aflah02@users.noreply.github.com>
2022-10-02 01:42:53 +0530

    Laplace Smoothing

---
+++ A2/preprocess_text.py
@@ -0,0 +1,116 @@
+import re
+from autocorrect import Speller
+import string
+from nltk.corpus import stopwords
+from nltk.corpus import wordnet as wn
+import matplotlib.pyplot as plt
+from nltk.stem import WordNetLemmatizer, PorterStemmer
+import nltk
+from nltk.tokenize import TweetTokenizer
+import numpy as np
+from tqdm.notebook import tqdm
+tqdm.pandas()
+from textblob import TextBlob
+# customregexes.py contains all the regex functions used for all parts
+from customregexes import *
+
+def tokenization(text):
+    twt_tk = TweetTokenizer()
+    return twt_tk.tokenize(text)
+
+wordnet_words = []
+
+for word in wn.words():
+    wordnet_words.append(word)
+
+words = set(wordnet_words)
+
+# Spelling correction using autocorrect
+def spelling_correction_autocorrect_full_sentence(text):
+    spell = Speller()
+    res = []
+    text = " ".join(text)
+    text = spell(text)
+    return tokenization(text)
+
+# Spelling correction using autocorrect
+def spelling_correction_autocorrect_per_token(text):
+    spell = Speller()
+    res = []
+    for token in text:
+        res.append(spell(token))
+    return res
+
+# Spelling correction using textblob
+def spelling_correction_textblob(text):
+    text = " ".join(text)
+    text = str(TextBlob(text))
+    return tokenization(text)
+
+def spelling_correction(text, method ='autocorrect_full'):
+    if method == 'autocorrect_full':
+        return spelling_correction_autocorrect_full_sentence(text)
+    elif method == 'autocorrect_token':
+        return spelling_correction_autocorrect_per_token(text)
+    elif method == 'textblob':
+        return spelling_correction_textblob(text)
+    else:
+        raise ValueError('Please use a valid method')
+
+#Here we can either do lemmatization or stemming, doing both at the same time will not be useful
+
+def lemmatization_text(text):
+    wn = WordNetLemmatizer()
+    lemmatized_text = []
+    for each in text:
+        lemmatized_word = wn.lemmatize(each)
+        lemmatized_text.append(lemmatized_word)
+    return ' '.join(lemmatized_text)
+
+def stemming_text(text):
+    stemmed_text = []
+    ps = PorterStemmer()
+    for each in text:
+        stemmed_word = ps.stem(each)
+        stemmed_text.append(stemmed_word)
+    return ' '.join(stemmed_text)
+
+def remove_punctuations(text):
+    puncts = string.punctuation
+    s = ""
+    for i in text:
+        if i not in puncts:
+            s += i
+    return s
+
+stop_words = stopwords.words("english")
+stop_words_without_punc = []
+for word in stop_words:
+    stop_words_without_punc.append(remove_punctuations(word))
+stop_words = stop_words_without_punc
+def remove_stopwords(text):
+    new_words = []
+    pattern = re.compile(r'\b(' + (r'|'.join(stop_words)) + r')\b\s*')
+    for word in text:
+        isMatch = pattern.match(word)
+        if not isMatch:
+            new_words.append(word)
+    return new_words
+
+def remove_whitespaces(text):
+    return re.sub(r'\s*\s', ' ', text)
+
+def remove_url_html(text):
+    urls = findURLsandHTML(text)
+    for i in urls:
+        text = re.sub(f"{i}", "", text)
+    return text
+
+def remove_users(text):
+    username = findUsernames(text)
+    for i in username:
+        text = re.sub(f"{i}", "", text)
+    return text
+
+def lowercase_text(text):
+    return lowercase(text)
---
+++ A2/smoothers.py
@@ -0,0 +1,20 @@
+class LMSmoothers:
+    """
+    Bigram Smoothing
+    """
+    def __init__(self, bigrams, vocab_len):
+        self.bigrams = bigrams
+        self.vocab_len = vocab_len
+        self.laplace_bigrams = {}
+
+    def laplace(self):
+        """
+        Laplace Smoothing
+        """
+        for bigram, freq in self.bigrams.items():
+            self.laplace_bigrams[bigram] = (freq + 1) / (self.vocab_len + len(self.bigrams))
+        return self.laplace_bigrams
+        
+    
+        
+
