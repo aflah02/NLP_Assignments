26d9f9685ed0aaee6590fb3091c774e6e0a8e00d
Aflah <72096386+aflah02@users.noreply.github.com>
2022-10-06 03:18:59 +0530

    New Smoothers and Smoothing Dumps

--- A2/smoothers.py
+++ A2/smoothers.py
@@ -2,18 +2,39 @@ class LMSmoothers:
     """
     Bigram Smoothing
     """
-    def __init__(self, bigrams, vocab_len):
+    def __init__(self, bigrams, vocab_len, unigram_counts, unigrams_probs = None):
         self.bigrams = bigrams
+        self.unigrams_probs = unigrams_probs
+        self.unigram_counts = unigram_counts
         self.vocab_len = vocab_len
         self.laplace_bigrams = {}
+        self.add_k_bigrams = {}
+        self.add_k_with_unigram_prior_bigrams = {}
 
     def laplace(self):
         """
         Laplace Smoothing
         """
         for bigram, freq in self.bigrams.items():
-            self.laplace_bigrams[bigram] = (freq + 1) / (self.vocab_len + len(self.bigrams))
+            self.laplace_bigrams[bigram] = (freq + 1) / (self.unigram_counts[bigram[0]] + len(self.bigrams))
         return self.laplace_bigrams
+    
+    def add_k(self, k):
+        """
+        Add-k Smoothing
+        """
+        for bigram, freq in self.bigrams.items():
+            self.add_k_bigrams[bigram] = (freq + k) / (self.unigram_counts[bigram[0]] + k * len(self.bigrams))
+        return self.add_k_bigrams
+
+    def add_k_with_unigram_prior(self, k):
+        """
+        Add-k Smoothing with Unigram Prior
+        """
+        m = k * self.vocab_len
+        for bigram, freq in self.bigrams.items():
+            self.add_k_with_unigram_prior_bigrams[bigram] = (freq + m * self.unigrams_probs[bigram[1]]) / (self.unigram_counts[bigram[0]] + m)
+        return self.add_k_with_unigram_prior_bigrams
         
         
     
