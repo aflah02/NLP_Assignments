20714e0f84821a41be430967bc5e8bdbb27c5ad4
stealth <generalom1234@gmail.com>
2022-10-09 20:02:06 +0530

    almost done

--- Neemesh_A2/testGenerated.py
+++ Neemesh_A2/testGenerated.py
@@ -11,19 +11,17 @@ def find_polarity_scores(txt):
     ls_word_sentiment_vader = np.array(ls_word_sentiment_vader)
     return ls_word_sentiment_vader
 
-ExtrinsicEvaluation("A").eval()
-
 print("--------------------------------------------------------------------------")
 
 neg_txt = []
-with open("A2/generated_sentences/neg_gen_only_ext.txt", 'r') as f:
+with open("Neemesh_A2/generated_sentences/neg_gen_only_ext.txt", 'r') as f:
     neg_txt = f.readlines()
 for each in range(len(neg_txt)):
     neg_txt[each] = neg_txt[each][:-2]
     neg_txt[each] = ' '.join(neg_txt[each].split()[:-1])
 
 pos_txt = []
-with open("A2/generated_sentences/pos_gen_only_ext.txt", 'r') as f:
+with open("Neemesh_A2/generated_sentences/pos_gen_only_ext.txt", 'r') as f:
     pos_txt = f.readlines()
 for each in range(len(pos_txt)):
     pos_txt[each] = pos_txt[each][:-2]
@@ -36,27 +34,28 @@ txt = neg_txt + pos_txt
 for i in range(500):
     txt[i] = [txt[i]]
     if i<250:
-        txt[i].append(neg_vaderScores[i])
+        txt[i].append(0)
     else:
-        txt[i].append(pos_vaderScores[i-250])
+        txt[i].append(1)
     txt[i] = np.array(txt[i])
 txt = np.array(txt)
 
 np.random.shuffle(txt)
 
+print("Results after using the prompts generated using vader polarity scores, and adding beta externally.")
 ExtrinsicEvaluation("B").eval(txt)
 
 print("--------------------------------------------------------------------------")
 
 neg_txt = []
-with open("A2/generated_sentences/neg_gen_add_numerator.txt", 'r') as f:
+with open("Neemesh_A2/generated_sentences/neg_gen_add_numerator.txt", 'r') as f:
     neg_txt = f.readlines()
 for each in range(len(neg_txt)):
     neg_txt[each] = neg_txt[each][:-2]
     neg_txt[each] = ' '.join(neg_txt[each].split()[:-1])
 
 pos_txt = []
-with open("A2/generated_sentences/pos_gen_add_numerator.txt", 'r') as f:
+with open("Neemesh_A2/generated_sentences/pos_gen_add_numerator.txt", 'r') as f:
     pos_txt = f.readlines()
 for each in range(len(pos_txt)):
     pos_txt[each] = pos_txt[each][:-2]
@@ -69,27 +68,28 @@ txt = neg_txt + pos_txt
 for i in range(500):
     txt[i] = [txt[i]]
     if i<250:
-        txt[i].append(neg_vaderScores[i])
+        txt[i].append(0)
     else:
-        txt[i].append(pos_vaderScores[i-250])
+        txt[i].append(1)
     txt[i] = np.array(txt[i])
 txt = np.array(txt)
 
 np.random.shuffle(txt)
 
+print("Results after using the prompts generated using vader polarity scores, adding beta in the numerator and externally.")
 ExtrinsicEvaluation("B").eval(txt)
 
 print("--------------------------------------------------------------------------")
 
 neg_txt = []
-with open("A2/generated_sentences/neg_gen_mul_numerator.txt", 'r') as f:
+with open("Neemesh_A2/generated_sentences/neg_gen_mul_numerator.txt", 'r') as f:
     neg_txt = f.readlines()
 for each in range(len(neg_txt)):
     neg_txt[each] = neg_txt[each][:-2]
     neg_txt[each] = ' '.join(neg_txt[each].split()[:-1])
 
 pos_txt = []
-with open("A2/generated_sentences/pos_gen_mul_numerator.txt", 'r') as f:
+with open("Neemesh_A2/generated_sentences/pos_gen_mul_numerator.txt", 'r') as f:
     pos_txt = f.readlines()
 for each in range(len(pos_txt)):
     pos_txt[each] = pos_txt[each][:-2]
@@ -102,27 +102,28 @@ txt = neg_txt + pos_txt
 for i in range(500):
     txt[i] = [txt[i]]
     if i<250:
-        txt[i].append(neg_vaderScores[i])
+        txt[i].append(0)
     else:
-        txt[i].append(pos_vaderScores[i-250])
+        txt[i].append(1)
     txt[i] = np.array(txt[i])
 txt = np.array(txt)
 
 np.random.shuffle(txt)
 
+print("Results after using the prompts generated using vader polarity scores, and multiplying beta with the numerator.")
 ExtrinsicEvaluation("B").eval(txt)
 
 print("--------------------------------------------------------------------------")
 
 neg_txt = []
-with open("A2/generated_sentences/neg_gen_div_denominator.txt", 'r') as f:
+with open("Neemesh_A2/generated_sentences/neg_gen_div_denominator.txt", 'r') as f:
     neg_txt = f.readlines()
 for each in range(len(neg_txt)):
     neg_txt[each] = neg_txt[each][:-2]
     neg_txt[each] = ' '.join(neg_txt[each].split()[:-1])
 
 pos_txt = []
-with open("A2/generated_sentences/pos_gen_div_denominator.txt", 'r') as f:
+with open("Neemesh_A2/generated_sentences/pos_gen_div_denominator.txt", 'r') as f:
     pos_txt = f.readlines()
 for each in range(len(pos_txt)):
     pos_txt[each] = pos_txt[each][:-2]
@@ -135,27 +136,28 @@ txt = neg_txt + pos_txt
 for i in range(500):
     txt[i] = [txt[i]]
     if i<250:
-        txt[i].append(neg_vaderScores[i])
+        txt[i].append(0)
     else:
-        txt[i].append(pos_vaderScores[i-250])
+        txt[i].append(1)
     txt[i] = np.array(txt[i])
 txt = np.array(txt)
 
 np.random.shuffle(txt)
 
+print("Results after using the prompts generated using vader polarity scores, and dividing beta from the denominator.")
 ExtrinsicEvaluation("B").eval(txt)
 
 print("--------------------------------------------------------------------------")
 
 neg_txt = []
-with open("A2/generated_sentences/neg_hf_prompts.txt", 'r') as f:
+with open("Neemesh_A2/generated_sentences/neg_hf_prompts.txt", 'r') as f:
     neg_txt = f.readlines()
 for each in range(len(neg_txt)):
     neg_txt[each] = neg_txt[each][:-2]
     neg_txt[each] = ' '.join(neg_txt[each].split()[:-1])
 
 pos_txt = []
-with open("A2/generated_sentences/pos_hf_prompts.txt", 'r') as f:
+with open("Neemesh_A2/generated_sentences/pos_hf_prompts.txt", 'r') as f:
     pos_txt = f.readlines()
 for each in range(len(pos_txt)):
     pos_txt[each] = pos_txt[each][:-2]
@@ -168,27 +170,28 @@ txt = neg_txt + pos_txt
 for i in range(500):
     txt[i] = [txt[i]]
     if i<250:
-        txt[i].append(neg_vaderScores[i])
+        txt[i].append(0)
     else:
-        txt[i].append(pos_vaderScores[i-250])
+        txt[i].append(1)
     txt[i] = np.array(txt[i])
 txt = np.array(txt)
 
 np.random.shuffle(txt)
 
+print("Results after using the prompts generated using vader polarity scores, and normalizing using perplexity.")
 ExtrinsicEvaluation("B").eval(txt)
 
 print("--------------------------------------------------------------------------")
 
 neg_txt = []
-with open("A2/generated_sentences/neg_ppl_normalized.txt", 'r') as f:
+with open("Neemesh_A2/generated_sentences/neg_ppl_normalized.txt", 'r') as f:
     neg_txt = f.readlines()
 for each in range(len(neg_txt)):
     neg_txt[each] = neg_txt[each][:-2]
     neg_txt[each] = ' '.join(neg_txt[each].split()[:-1])
 
 pos_txt = []
-with open("A2/generated_sentences/pos_ppl_normalized.txt", 'r') as f:
+with open("Neemesh_A2/generated_sentences/pos_ppl_normalized.txt", 'r') as f:
     pos_txt = f.readlines()
 for each in range(len(pos_txt)):
     pos_txt[each] = pos_txt[each][:-2]
@@ -201,12 +204,47 @@ txt = neg_txt + pos_txt
 for i in range(500):
     txt[i] = [txt[i]]
     if i<250:
-        txt[i].append(neg_vaderScores[i])
+        txt[i].append(0)
     else:
-        txt[i].append(pos_vaderScores[i-250])
+        txt[i].append(1)
     txt[i] = np.array(txt[i])
 txt = np.array(txt)
 
 np.random.shuffle(txt)
 
+print("Results after using the prompts generated using HuggingFace polarity scores, and adding beta externally.")
+ExtrinsicEvaluation("B").eval(txt)
+
+print("--------------------------------------------------------------------------")
+
+neg_txt = []
+with open("Neemesh_A2/generated_sentences/neg_vader_textblob.txt", 'r') as f:
+    neg_txt = f.readlines()
+for each in range(len(neg_txt)):
+    neg_txt[each] = neg_txt[each][:-2]
+    neg_txt[each] = ' '.join(neg_txt[each].split()[:-1])
+
+pos_txt = []
+with open("Neemesh_A2/generated_sentences/pos_vader_textblob.txt", 'r') as f:
+    pos_txt = f.readlines()
+for each in range(len(pos_txt)):
+    pos_txt[each] = pos_txt[each][:-2]
+    pos_txt[each] = ' '.join(pos_txt[each].split()[:-1])
+
+neg_vaderScores = find_polarity_scores(neg_txt)
+pos_vaderScores = find_polarity_scores(pos_txt)
+
+txt = neg_txt + pos_txt
+for i in range(500):
+    txt[i] = [txt[i]]
+    if i<250:
+        txt[i].append(0)
+    else:
+        txt[i].append(1)
+    txt[i] = np.array(txt[i])
+txt = np.array(txt)
+
+np.random.shuffle(txt)
+
+print("Results after using the prompts generated using Vader Polarity Scores, using beta externally, and textblob autocorrect.")
 ExtrinsicEvaluation("B").eval(txt)
